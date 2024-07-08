1bb8cc6f89d69476769e068de2614f6a1fbddee8
maliha-wiva <maliha.wiva@gmail.com>
2024-07-08 21:05:47 +0500

    add

---
+++ Gitpractice/Gitpractice/GitpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitpracticeApp.swift
+//  Gitpractice
+//
+//  Created by Maliha on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
